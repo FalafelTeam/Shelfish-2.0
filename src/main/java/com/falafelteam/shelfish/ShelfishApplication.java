72efedddaf9f6891e6f1a974831946d759b61065
John Doe <saitov66@gmail.com>
2018-03-30 02:02:41 +0300

    Spring Booted

---
+++ src/main/java/com/falafelteam/shelfish/ShelfishApplication.java
@@ -0,0 +1,12 @@
+package com.falafelteam.shelfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfishApplication.class, args);
+	}
+}
---
+++ src/test/java/com/falafelteam/shelfish/ShelfishApplicationTests.java
@@ -0,0 +1,16 @@
+package com.falafelteam.shelfish;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShelfishApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
